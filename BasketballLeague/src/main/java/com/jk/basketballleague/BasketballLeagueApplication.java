a0398dfe2b87892949641720bbb241ab796e4d4b
jakub <ookubaoo2@gmail.com>
2023-06-18 22:45:38 +0200

    Test

---
+++ BasketballLeague/src/main/java/com/jk/basketballleague/BasketballLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.jk.basketballleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballLeagueApplication.class, args);
+	}
+
+}
