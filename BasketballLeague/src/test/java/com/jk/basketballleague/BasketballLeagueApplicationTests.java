bbcf2064cc8ff3f0723f89f8e91327d0efbb7e17
jakub <ookubaoo2@gmail.com>
2023-06-18 22:35:45 +0200

    Initial commit

---
+++ BasketballLeague/src/test/java/com/jk/basketballleague/BasketballLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.basketballleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
